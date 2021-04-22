bdb4b22d279df36c9b6f0c3cff7dadf38af89dec
rayyiu <rayyiu66@gmail.com>
2021-04-22 03:31:30 +0000

    updated all seeds for new tokens

--- db/seeds/04_widgets.sql
+++ db/seeds/04_widgets.sql
@@ -1,13 +1,36 @@
 INSERT INTO widgets (rarity_id, subcategory_id, name, MSRP_cents, for_sale_by_owner, current_sell_price_cents, hash, description, imgUrl)
-VALUES (4, 3, 'Blue Eyes White Dragon', 4999, true, 9999, 'hashno1', 'This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.', 'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/177279.jpg'),
-(3, 3, 'Dark Magician', 1499, true, 2052, 'hashno2', 'The ultimate wizard in terms of attack and defense.', 'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/1047653.jpg'),
-(2, 3, 'Kuriboh', 1299, true, 1878, 'hashno3', 'During damage calculation, if your opponents monster attacks (Quick Effect): You can discard this card; you take no battle damage from that battle.', 'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/1104621.jpg'),
-(1, 3, 'Dark Magician Girl', 299, true, 358, 'hashno4', 'Gains 300 ATK for every "Dark Magician" or "Magician of Black Chaos" in the GY.', 'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/1665595.jpg'),
-(2, 1, 'Pikachu', 999, true, 1480, 'hashno5', 'Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'),
-(3, 1, 'Raichu', 1999, true, 2699, 'hashno6', 'Its long tail serves as a ground to protect itself from its own high-voltage power.', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png'),
-(3, 1, 'Snorlax', 2499, true, 3599, 'hashno7', 'It is not satisfied unless it eats over 880 pounds of food every day. When it is done eating, it goes promptly to sleep.', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png'),
-(4, 1, 'Mewtwo', 5999, true, 8999, 'hashno8', 'Its DNA is almost the same as Mews. However, its size and disposition are vastly different.', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png'),
-(4, 2, 'Black Lotus', 500000, true, 999999, 'hashno9', 'Tap, Sacrifice Black Lotus: Add three mana of any one color.', 'https://deckbox.org/system/images/mtg/cards/600.jpg'),
-(1, 2, 'Island', 30, true, 18, 'hashno10', 'T: Add U to your mana pool', 'https://deckbox.org/system/images/mtg/cards/2016212.jpg'),
-(2, 2, 'Badlands', 999, true, 16499, 'hashno11', 'T: Add B or R to your mana pool', 'https://deckbox.org/system/images/mtg/cards/1376.jpg'),
-(3, 2, 'Birds of Paradise', 1399, true, 3199, 'hashno12', 'T: Add one mana of any color to your mana pool.', 'https://deckbox.org/system/images/mtg/cards/2004212.jpg');
+VALUES 
+(4, 1, 'PIKACHU ULTRARARE', 500000, true, 1150000, 'hashno0', 'the iconic pokemon, video-game and culture icon, and our first NFT', 'https://64.media.tumblr.com/9e10e8601a9e24632c3b98ba799351cf/c4536a8c77a9b6f2-3a/s1280x1920/cab867d7865890ed4abfb552d99d9a7751b0975c.jpg'),
+(4, 2, 'MEWTWO KING ULTRARARE', 250000, true, 40000, 'hashno1', 'an iconic gamer for us SmashHeads', 'https://64.media.tumblr.com/c3e4c58ec2f21e3e5a2bf0108c8c71f1/c4536a8c77a9b6f2-4b/s1280x1920/de5dcb8fd4a461bf2730eddb25d72339602f6221.jpg'),
+(4, 4, 'FOX ULTRARARE', 140000, true, 190000, 'hashno2', 'we need to nerf Fox, hes too fast', 'https://64.media.tumblr.com/efa4e6f609462f72f8d9f5b9e526a51b/c4536a8c77a9b6f2-bc/s1280x1920/ee541b7fb60886e20352b84db19dcad10bf1e18c.jpg'),
+(3, 3, 'The Floss', 15000, true, 9000, 'hashno3', 'Dont do the dance boy', 'https://i.imgur.com/Nz3X45I.gif'),
+(3, 3, 'Shufflin', 8000, true, 5000, 'hashno4', 'not just for your kids to do', 'https://i.imgur.com/C6VceDe.gif'),
+(3, 3, '180 NoScope', 110000, true, 140000, 'hashno5', 'EZ KILL', 'https://64.media.tumblr.com/1241bb30aeab79e9da619a353858df6c/85aff3af59ba4174-23/s500x750/583d62478158e7cab383d465eb17f59df549b5f4.gifv'),
+(3, 3, 'The Shine', 100000, true, 15000, 'hashno6', 'Mission Complete', 'https://i.imgur.com/qdhKB49.gif'),
+(2, 4, 'Doom', 30000, true, 35000, 'hashno7', 'A piece of design art from the original Doom', 'https://i.imgur.com/VFT0CTw.jpg'),
+(2, 4, 'Glados', 40000, true, 20000, 'hashno8', 'You will be fed and there will be cake', 'https://i.imgur.com/qjNgY2N.jpeg'),
+(4, 4, 'The Companion Cube', 20000, true, 70000, 'hashno9', 'The Enrichment Center reminds you that the Weighted Companion Cube cannot speak.', 'https://i.imgur.com/Xgg8OVk.jpg'),
+(1, 4, 'Big Daddy', 2500, true, 2200, 'hashno10', 'Youre safe now. Big Daddy is here', 'https://i.imgur.com/SrcIqOL.jpg'),
+(4, 4, 'BUYING GF', 99999, true, 1000000, 'hashno11', 'Will trade full Rune+GP for gf', 'https://i.imgur.com/SEE5G4N.jpg')
+
+
+
+
+
+
+
+
+-- (4, 3, 'Blue Eyes White Dragon', 4999, true, 9999, 'hashno1', 'This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.', 'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/177279.jpg'),
+-- (3, 3, 'Dark Magician', 1499, true, 2052, 'hashno2', 'The ultimate wizard in terms of attack and defense.', 'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/1047653.jpg'),
+-- (2, 3, 'Kuriboh', 1299, true, 1878, 'hashno3', 'During damage calculation, if your opponents monster attacks (Quick Effect): You can discard this card; you take no battle damage from that battle.', 'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/1104621.jpg'),
+-- (1, 3, 'Dark Magician Girl', 299, true, 358, 'hashno4', 'Gains 300 ATK for every "Dark Magician" or "Magician of Black Chaos" in the GY.', 'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/1665595.jpg'),
+-- (2, 1, 'Pikachu', 999, true, 1480, 'hashno5', 'Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'),
+-- (3, 1, 'Raichu', 1999, true, 2699, 'hashno6', 'Its long tail serves as a ground to protect itself from its own high-voltage power.', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png'),
+-- (3, 1, 'Snorlax', 2499, true, 3599, 'hashno7', 'It is not satisfied unless it eats over 880 pounds of food every day. When it is done eating, it goes promptly to sleep.', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png'),
+-- (1, 2, 'Fox', 25000, true, 25000, 'hashno6.5', 'Shine to backair upsmash', 'https://scontent.fyvr2-1.fna.fbcdn.net/v/t1.15752-9/174408096_927045221458927_4871565419239511951_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=ae9488&_nc_ohc=vpgTHySryU4AX8pxJer&_nc_ht=scontent.fyvr2-1.fna&oh=2ac0f4b827679a841e5cad8a39e05bb9&oe=60A4A557'),
+-- (2, 2, 'Fox Shine', 15000, true, 15000, 'hashno6.75', 'So fast', 'https://i.imgur.com/qdhKB49.gif'),
+-- (4, 1, 'Mewtwo', 5999, true, 8999, 'hashno8', 'Its DNA is almost the same as Mews. However, its size and disposition are vastly different.', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png'),
+-- (4, 2, 'Black Lotus', 500000, true, 999999, 'hashno9', 'Tap, Sacrifice Black Lotus: Add three mana of any one color.', 'https://deckbox.org/system/images/mtg/cards/600.jpg'),
+-- (1, 2, 'Island', 30, true, 18, 'hashno10', 'T: Add U to your mana pool', 'https://deckbox.org/system/images/mtg/cards/2016212.jpg'),
+-- (2, 2, 'Badlands', 999, true, 16499, 'hashno11', 'T: Add B or R to your mana pool', 'https://deckbox.org/system/images/mtg/cards/1376.jpg'),
+-- (3, 2, 'Birds of Paradise', 1399, true, 3199, 'hashno12', 'T: Add one mana of any color to your mana pool.', 'https://deckbox.org/system/images/mtg/cards/2004212.jpg');
